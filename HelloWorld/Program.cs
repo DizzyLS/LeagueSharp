3a3aa4b58562b09cbe8d874374efdd6262e7f1cc
DizzyLS <djc04@outlook.com>
2015-01-10 17:46:18 +1100

    Added stuff

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game.PrintChat("DizzyEzreal 1.0 Loaded. Have fun!");
+        }
+    }
+}
